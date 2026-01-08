35785fbcd8b0d7054113717601af12c5f116a4ed
Aby Dramé <aby.drm@gmail.com>
2026-01-08 15:43:20 +0000

    created macro local_sales_macro.sql

--- models/staging/carrefour/stg_crf_hyp_sales_fr_data.sql
+++ models/staging/carrefour/stg_crf_hyp_sales_fr_data.sql
@@ -1,14 +1,6 @@
 with source as (
 
-    select
-        cast(barcode_ean13 as string)        as barcode,
-        cast(site_key as int64)              as site_key,
-        cast(product_CATEGORY as string)     as product_category,
-        cast(pdt_SUB_CATEGORY as string)     as product_sub_category,
-        DATE                                 as sales_date,
-        cast(CA as float64)                  as ca
-
-    from {{ source('dbt_carrefour', 'crf_hyp_sales_fr_data') }}
+    {{ local_sales('crf_hyp_sales_fr_data') }}
 
 )
 
